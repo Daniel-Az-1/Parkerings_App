7b307193dc60be37400bd76e30c75d63a79a8e67
Daniel <azary.daniel@gmail.com>
2024-10-21 18:27:46 +0200

    Detta e final version av logo

--- logo.py
+++ logo.py
@@ -2,5 +2,3 @@ def show_logo():
     print("_____________________________________\n")
     print("  **  Välkommen till Quick Park  **  ")
     print("_____________________________________")
-
-show_logo()
