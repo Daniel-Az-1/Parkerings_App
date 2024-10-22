4d86f6908a59809995875d56870b2cb227b86237
Daniel <azary.daniel@gmail.com>
2024-10-22 19:49:02 +0200

    Jag har definierat modul pris_lista_kort nu

---
+++ pris_lista_kort.py
@@ -0,0 +1,8 @@
+def show_pris_short_time():
+    pris_list = {"En timme": 20, "24 timmar": 200}
+    line_number = 1
+    print("Observera att du debiteras per minut om du parkerar kortare eller längre än en timme.")
+    print("\nPrislista för kortvarig parkering:")
+    for time, pris in pris_list.items():
+        print(f"{line_number}) {time} = {pris} Kr")
+        line_number += 1
