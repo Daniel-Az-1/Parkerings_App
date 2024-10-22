2a1761018ae6ca5550064a006a47257f946b3e36
Daniel <azary.daniel@gmail.com>
2024-10-22 19:47:19 +0200

    Jag har definierat modul pris_lista_long

---
+++ pris_lista_long.py
@@ -0,0 +1,8 @@
+def show_pris_long_time():
+    pris_list = {"1 månad": 500, "3 månader": 1400, "6 månader": 2800, "12 månader": 5500}
+    line_number = 1
+    print("Observera att du hyr en specifik parkeringsplats med ett nummer och du får endast parkera på den platsen.")
+    print("\nPrislista för långvarig parkering:")
+    for time, pris in pris_list.items():
+        print(f"{line_number}) {time} = {pris} Kr")
+        line_number += 1
