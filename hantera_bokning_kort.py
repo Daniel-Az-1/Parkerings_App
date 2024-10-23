adec984414ca3490363f1a9f962bdc4b2e7f3860
Daniel <azary.daniel@gmail.com>
2024-10-23 23:57:32 +0200

    Jag har uppdaterat hantera_bokning_long och hantera_bokning_kort modukerna

---
+++ hantera_bokning_kort.py
@@ -0,0 +1,29 @@
+import bekrafta_tiden_kort
+
+def manage_booking():
+    while True:
+        print("Ange den tid du önskar parkera i minuter:")
+        user_choice = input("Vänligen ange ett positivt heltal för antalet minuter (t.ex. 30, 60, 90): ")
+
+        
+        if user_choice.isdigit() and int(user_choice) > 0:
+            user_choice = int(user_choice) 
+            print(f"Du har valt att parkera i {user_choice} minuter.")
+            bekrafta_tiden_kort.confirm_the_time(user_choice)
+            return user_choice
+        else:
+            print("Ogiltig inmatning. Vänligen ange ett positivt heltal för att fortsätta.")
+            
+        while True:
+            print("Välj ett av följande alternativ:")
+            print("1) Ange ny parkeringstid")
+            print("2) Avsluta bokningen")
+            retry_choice = input("Ange ditt val: ")
+
+            if retry_choice == "1":
+                break
+            elif retry_choice == "2":
+                print("Bokningen har avslutats. Tack för att du använde Quick Park! Välkommen åter!")
+                return
+            else:
+                print("Ogiltigt val, försök igen.")
---
+++ hantera_bokning_long.py
@@ -0,0 +1,29 @@
+import bekrafta_tiden_long
+
+def manage_booking():
+    while True:
+        print("Ange hur många månader du vill hyra parkeringsplatsen:")
+        user_choice = input("Välj mellan 1, 3, 6 eller 12 månader: ")
+
+        
+        if user_choice.isdigit() and int(user_choice) in [1, 3, 6, 12]:
+            user_choice = int(user_choice) 
+            print(f"Du har valt att hyra parkeringen i {user_choice} månad/månader.")
+            bekrafta_tiden_long.confirm_the_time(user_choice)
+            return user_choice 
+        else:
+            print("Ogiltigt val. Du kan välja 1, 3, 6 eller 12 månader.")
+        
+        while True:
+            print("Välj ett av följande alternativ:")
+            print("1) Ange ett nytt antal månader")
+            print("2) Avsluta bokningen")
+            retry_choice = input("Ange ditt val: ")
+
+            if retry_choice == "1":
+                break
+            elif retry_choice == "2":
+                print("Bokningen har avslutats. Tack för att du använde Quick Park!")
+                return
+            else:
+                print("Ogiltigt val, försök igen.")
