37cb63ddfc8104398f899b75787cf620597b3d99
Daniel <azary.daniel@gmail.com>
2024-10-25 16:55:59 +0200

    vi har nu 2 moduler som hanterar uppdatringen av parkeringslistor uppdatera_lediga_platser_kort.py och uppdatera_lediga_platser_long.py

---
+++ uppdatera_lediga_platser_long.py
@@ -0,0 +1,17 @@
+
+def update_available_parking_spaces(space_nummer, ny_status, confirmed_number_of_months=None, regnummer=None):
+
+    if ny_status == "upptagen" and regnummer:
+        import visa_langvariga_str
+        visa_langvariga_str.parking_spaces[space_nummer] = [ny_status, regnummer]
+        print(f"Du har hyrt parkeringsplatsen {space_nummer} för {confirmed_number_of_months} månad/månader. \nTack för att du har använt Quick Park, hejdå!")
+        return 
+    elif ny_status == "ledig":
+        import visa_langvariga_str
+        visa_langvariga_str.parking_spaces[space_nummer] = [ny_status]
+        print("Uppdatering av listan av långa parkeringsplatser har lyckats !")
+        return
+    else:
+        print("Ett tekniskt fel har inträffat! vänligen kontakta kundtjänsten med telefonnummer 0737826801 för att få hjälp")
+
+
