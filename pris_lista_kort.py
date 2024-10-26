62790d58de730d11d6b0708acafd82faa573950b
Daniel <azary.daniel@gmail.com>
2024-10-26 16:21:29 +0200

    Jag har uppdaterat pris_lista_kort.py och pris_lista_long.py de funkar felfri nu

--- pris_lista_kort.py
+++ pris_lista_kort.py
@@ -1,9 +1,13 @@
-pris_list = {"En timme": 20, "24 timmar":"450"}
+pris_list = {1:20, 24:450}
 
 def show_pris_short_time():
     line_number = 1
     print("Observera att du debiteras per minut om du parkerar kortare eller längre än en timme.")
     print("\nPrislista för kortvarig parkering:")
     for time, pris in pris_list.items():
-        print(f"{line_number}) {time} = {pris} Kr")
+        if time == 1:
+            time_text = "timme"
+        else:
+            time_text = "timmar"
+        print(f"{line_number}) {time} {time_text} = {pris} Kr")
         line_number += 1
--- pris_lista_long.py
+++ pris_lista_long.py
@@ -1,10 +1,13 @@
-pris_list = {"1 månad": 500, "3 månader": 1400, "6 månader": 2800, "12 månader": 5500}
+pris_list = {1:500, 3:1400, 6:2800, 12: 5500}
 
 def show_pris_long_time():
     line_number = 1
     print("Observera att du hyr en specifik parkeringsplats med ett nummer och du får endast parkera på den platsen.")
     print("\nPrislista för långvarig parkering:")
     for time, pris in pris_list.items():
-        print(f"{line_number}) {time} = {pris} Kr")
+        if time == 1:
+            time_text = "månad"
+        else:
+            time_text = "månader"
+        print(f"{line_number}) {time} {time_text} = {pris} Kr")
         line_number += 1
-
