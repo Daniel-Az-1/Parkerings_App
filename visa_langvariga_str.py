54d994c834c31fa8791e3de8a64b4195121f2650
Daniel <azary.daniel@gmail.com>
2024-10-26 19:23:48 +0200

    Jag har uppdaterat visa_langvariga_str.py med User_Maimuns koder nu

--- visa_langvariga_str.py
+++ visa_langvariga_str.py
@@ -1,3 +1,23 @@
-parking_spaces = {"1": ["ledig"], "2": ["upptagen", "Bah73Y"], "3": ["ledig"]}
-
+parking_spaces = {
+        "1":["ledig"],
+        "2":["ledig"],
+        "3":["ledig"],
+        "4":["upptagen","ABC676"],
+        "5":["ledig"],
+        "6":["ledig"],
+        "7":["ledig"]
+    }
+     
 def show_available_parking_long():
+    available = False
+    result_str = "\nTillgängliga långvariga parkeringsplatser:\n"
+
+    for key in parking_spaces:
+            if parking_spaces[key] == ["ledig"]:
+                result_str += f"Parkerings plats: {key}\n"
+                available = True
+
+    if not available:
+        return "Inga långvariga parkeringsplatser är lediga."
+    else:
+        return print(result_str)
