324fb8d813bc5eca923f87366cd04d90e4b5284b
Daniel <azary.daniel@gmail.com>
2024-10-26 19:29:49 +0200

    Jag har uppdaterat visa_kortvariga_str,py med user_Maimuns koder nu

--- visa_kortvariga_str.py
+++ visa_kortvariga_str.py
@@ -1,2 +1,24 @@
-parking_spaces = {"1": ["ledig"], "2": ["upptagen", "Bah73Y"], "3": ["ledig"]}
-def show_available_parking_short():
+parking_spaces = {
+        "1":["ledig"],
+        "2":["ledig"],
+        "3":["ledig"],
+        "4":["ledig"],
+        "5":["upptagen","HJU531"],
+        "6":["ledig"],
+        "7":["ledig"]
+    }
+    
+def show_available_parking_kort():
+    available = False
+    result_str = "\nLediga kortvariga parkeringsplatser:\n"
+    
+    for key in parking_spaces:
+        if parking_spaces[key] == ["ledig"]:
+            result_str += f"Parkerings plats: {key}\n"
+            available = True
+
+    if not available:
+        return "Inga kortvariga parkeringsplatser är lediga."
+    else:
+        return print(result_str)
+    
