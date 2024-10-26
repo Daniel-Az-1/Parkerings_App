115cfab005ab2bb1b22a6978f349277dd35b47fd
Daniel <azary.daniel@gmail.com>
2024-10-26 20:34:26 +0200

    Testade en gång till allt fungerar behover smo andringar i meddelandet som visas och lite annat

--- simulera_betalning_long.py
+++ simulera_betalning_long.py
@@ -9,7 +9,6 @@ def confirm_payment(confirmed_number_of_months):
         if user_choice == "ja":
             print("Du kan betala med MasterCard eller Visa.")
             
-            # Betalningsprocess med möjlighet att försöka igen vid fel
             while True:
                 card_type = input("Välj 'Master' eller 'Visa': ").lower()
                 if card_type in ["master", "visa"]:
@@ -22,14 +21,12 @@ def confirm_payment(confirmed_number_of_months):
                             print(f"Betalningen lyckades! Du har debiterats med {prise} kr.")
                             car_register_number = input("Ange bilens registreringsnummer: ")
 
-                            # Visa lediga platser med möjlighet att välja rätt plats
                             while True:
                                 import visa_langvariga_str
                                 visa_langvariga_str.show_available_parking_long()
 
                                 select_parking_space = input("Välj en ledig plats från listan: ")
 
-                                # Kontrollera om vald plats är ledig
                                 if select_parking_space in visa_langvariga_str.parking_spaces and visa_langvariga_str.parking_spaces[select_parking_space][0]== "ledig":
                                     new_status = "upptagen"
                                     import uppdatera_lediga_platser_long
